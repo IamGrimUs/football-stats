be2caed26551a4ed4031f14d50fe7afd40d19074
r_dbl_l <iamgrimus@gmail.com>
2024-03-09 23:57:41 -0800

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { MatchReader } from './MatchReader'
+import { CsvFileReader } from './CsvFileReader'
+import { Summary } from './Summary'
+import { WinsAnalysis } from './analyzers/WinsAnalysis'
+import { ConsoleReport } from './reportTargets/consoleReport'
+import { HtmlReport } from './reportTargets/HtmlReport'
+
+const csvFileReader = new CsvFileReader('football.csv')
+const matchReader = new MatchReader(csvFileReader)
+matchReader.load()
+
+const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport())
+summary.buildAndPrintReport(matchReader.matches)
